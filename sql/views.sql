3ed668c26b7eced3629db79cde094a7948ac55b5
Mohamed Sharfan <sharfansaleem72@gmail.com>
2025-10-24 22:17:06 +0530

    delete insert into code

--- sql/views.sql
+++ sql/views.sql
@@ -295,3 +295,51 @@ FROM (
 
 
 
+
+
+
+
+
+CREATE OR REPLACE VIEW attendance_summary_by_student AS
+SELECT
+    s.reg_no,
+    CONCAT(u.f_name, ' ', u.l_name) AS student_name,
+    a.course_code,
+    cu.session_hour,
+    SUM(
+        CASE
+            WHEN a.status = 'Present' THEN cu.session_hour / COUNT(a.week_no)
+            WHEN a.status = 'Medical' AND m.status = 'Approved' THEN cu.session_hour / COUNT(a.week_no)
+            ELSE 0
+        END
+    ) AS attended_hours,
+    
+    ROUND(
+        SUM(
+            CASE
+                WHEN a.status = 'Present' THEN cu.session_hour / COUNT(a.week_no)
+                WHEN a.status = 'Medical' AND m.status = 'Approved' THEN cu.session_hour / COUNT(a.week_no)
+                ELSE 0
+            END
+        ) / cu.session_hour * 100, 2
+    ) AS attendance_percentage,
+
+    CASE
+        WHEN ROUND(
+            SUM(
+                CASE
+                    WHEN a.status = 'Present' THEN cu.session_hour / COUNT(a.week_no)
+                    WHEN a.status = 'Medical' AND m.status = 'Approved' THEN cu.session_hour / COUNT(a.week_no)
+                    ELSE 0
+                END
+            ) / cu.session_hour * 100, 2
+        ) >= 80 THEN 'Eligible'
+        ELSE 'Not Eligible'
+    END AS eligibility
+
+FROM attendance a
+JOIN student s ON a.reg_no = s.reg_no
+JOIN user u ON s.reg_no = u.id
+LEFT JOIN medical m ON a.ref_no = m.ref_no
+JOIN course_unit cu ON a.course_code = cu.course_code
+GROUP BY s.reg_no, a.course_code, student_name, cu.session_hour;
