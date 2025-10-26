c71da01ca8981eef88d0b073727db9ca7cb876ef
razim2003 <raazim2003@gmail.com>
2025-10-26 19:49:58 +0530

    add privileges

--- sql/users.sql
+++ sql/users.sql
@@ -1,9 +1,10 @@
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';
 GRANT ALL PRIVILEGES ON tecmis.* TO 'admin'@'localhost' WITH GRANT OPTION;
-
+FLUSH PRIVILEGES;
 
 CREATE USER 'dean'@'localhost' IDENTIFIED BY 'dean123';
 GRANT ALL PRIVILEGES ON tecmis.* TO 'dean'@'localhost';
+FLUSH PRIVILEGES;
 
 
 CREATE USER 'techofficer'@'localhost' IDENTIFIED BY 'tech123';
@@ -14,18 +15,18 @@ GRANT SELECT ON tecmis.attendance_summary TO 'techofficer'@'localhost';
 GRANT SELECT ON tecmis.attendance_summary_by_course TO 'techofficer'@'localhost';
 GRANT SELECT ON tecmis.attendance_summary_by_student TO 'techofficer'@'localhost';
 GRANT SELECT ON tecmis.attendance_summary_pivot TO 'techofficer'@'localhost';
+FLUSH PRIVILEGES;
 
 
 
 CREATE USER 'student'@'localhost' IDENTIFIED BY 'student123';
-
 GRANT SELECT ON tecmis.attendance_summary TO 'student'@'localhost';
 GRANT SELECT ON tecmis.attendance_summary_by_student TO 'student'@'localhost';
 GRANT SELECT ON tecmis.attendance_combined_theory_practical TO 'student'@'localhost';
 GRANT SELECT ON tecmis.attendance_detail_by_course TO 'student'@'localhost';
 GRANT SELECT ON tecmis.attendance_summary_by_course TO 'student'@'localhost';
 GRANT SELECT ON tecmis.attendance_summary_pivot TO 'student'@'localhost';
-
 GRANT SELECT ON tecmis.student_final_grades_student_version TO 'student'@'localhost';
 GRANT SELECT ON tecmis.student_grades_pivot_view_student_version TO 'student'@'localhost';
 GRANT SELECT ON tecmis.student_gpa TO 'student'@'localhost';
+FLUSH PRIVILEGES;
