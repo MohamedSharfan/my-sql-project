6679216a8c70696a90ca6e0f41291b0615bb49b3
Mohamed Sharfan <sharfansaleem72@gmail.com>
2025-10-26 22:13:36 +0530

    add testing queries

---
+++ sql/test-queries.sql
@@ -0,0 +1,21 @@
+CALL GetAttendanceByCourse('ICT1222');
+
+
+CALL GetAttendanceByStudent('TG/2023/1786');
+
+
+CALL GetAttendanceByStudentAndCourse('TG/2023/1786','ICT1222');
+
+
+CALL GetAttendanceCombined('TG/2023/1786','ICT1253');
+
+
+CALL GetAttendancePracticalOnly('TG/2023/1786','ICT1253');
+
+
+CALL GetAttendanceTheoryOnly('TG/2023/1786','ICT1253');
+
+
+CALL GetCourseAttendanceDetails('ICT1253');
+
+
